8c0afb18746e5a49bdf7ec717b3c73c9c0317eac
Gabriel Rodrigues <gabriel.rodrigues.brito@gmail.com>
2024-06-19 19:01:13 -0300

    configuração sequelize + dotenv

--- api/index.js
+++ api/index.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
 const app = express();
 
-app.listen(3000, err => {
+app.use(express.json());
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
+});
+
+sequelize.authenticate()
+    .then(() => console.log('Conexão com o banco de dados estabelecida.'))
+    .catch(err => console.error('Não foi possível conectar ao banco de dados: ', err));
+
+app.listen(process.env.SERVER_PORT, err => {
     if (err) {
-        console.log("Erro ao rodar o servidor: " + err);
+        console.log('Erro ao rodar o servidor: ', err);
         return;
     }
 
-    console.log("Servidor rodando na porta 3000");
+    console.log(`Servidor rodando na porta ${process.env.SERVER_PORT}`);
 });
