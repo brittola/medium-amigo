fd0e3ffa246f59bf023c67a4e73e3e505b2c8591
Gabriel Rodrigues <gabriel.rodrigues.brito@gmail.com>
2024-06-19 23:10:37 -0300

    jwt e rota de login

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -1,20 +1,23 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const { hasEmptyField } = require('../utils/UserValidator');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET;
 
 class UsersController {
 
     async create(req, res) {
         const { name, email, password } = req.body;
 
-        try {
-            if (hasEmptyField(name, email, password)) {
-                res.status(400).json({
-                    error: "Um ou mais campos faltando"
-                });
-                return;
-            }
+        if (hasEmptyField(name, email, password)) {
+            res.status(400).json({
+                error: "Um ou mais campos faltando"
+            });
+            return;
+        }
 
+        try {
             const user = await User.findOne({ where: { email } });
 
             if (user) {
@@ -32,6 +35,44 @@ class UsersController {
         } catch (err) {
             console.log(err);
             res.status(500).send("Erro ao criar usuário");
+        }
+    }
+
+    async auth(req, res) {
+        const { email, password } = req.body;
+
+        if (hasEmptyField(email, password)) {
+            res.status(400).send("Um ou mais campos faltando");
+            return;
+        }
+
+        try {
+            const user = await User.findOne({ email });
+
+            if (user) {
+                if (bcrypt.compareSync(password, user.password)) {
+                    const MONTH = 30 * 24 * 60 * 60;
+
+                    jwt.sign({ id: user.id, email }, secret, { expiresIn: MONTH }, (err, token) => {
+                        if (err) {
+                            console.log(err);
+                            res.sendStatus(500);
+                        } else {
+                            res.json({ token });
+                        }
+                    });
+
+                } else {
+                    res.status(401).send("E-mail ou senha incorreta");
+                }
+
+                return;
+            }
+
+            res.status(400).status("Não existe usuário cadastrado com este e-mail");
+        } catch (err) {
+            console.log(err);
+            res.status(500).send("Erro ao autenticar usuário");
         }
     }
 }
--- api/routes/UserRoutes.js
+++ api/routes/UserRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const UsersController = require('../controllers/UsersController');
 const router = express.Router();
 
-router.post('/users', UsersController.create)
+router.post('/users', UsersController.create);
+router.post('/users/auth', UsersController.auth);
 
 module.exports = router;
