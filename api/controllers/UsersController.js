35edd40814bd2e3487197e4331603a64c338331a
Gabriel Rodrigues <gabriel.rodrigues.brito@gmail.com>
2024-06-21 09:29:58 -0300

    hotfix: rota sem resposta quando usuário não existe

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -45,27 +45,29 @@ class UsersController {
         try {
             const user = await User.findOne({ where: { email, is_deleted: false } });
 
-            if (user) {
-                if (bcrypt.compareSync(password, user.password)) {
-                    const MONTH = 30 * 24 * 60 * 60;
-
-                    jwt.sign({ id: user.id, email }, secret, { expiresIn: MONTH }, (err, token) => {
-                        if (err) {
-                            console.log(err);
-                            res.sendStatus(500);
-                        } else {
-                            res.json({ token });
-                        }
-                    });
-
-                } else {
-                    res.status(401).send("E-mail ou senha incorreta");
-                }
-
+            if (!user) {
+                res.status(400).send("Não existe usuário cadastrado com este e-mail");
                 return;
             }
 
-            res.status(400).status("Não existe usuário cadastrado com este e-mail");
+            if (bcrypt.compareSync(password, user.password)) {
+                const MONTH = 30 * 24 * 60 * 60;
+
+                jwt.sign({ id: user.id, email }, secret, { expiresIn: MONTH }, (err, token) => {
+                    if (err) {
+                        console.log(err);
+                        res.sendStatus(500);
+                    } else {
+                        res.json({ token });
+                    }
+                });
+
+                return;
+
+            }
+
+            res.status(401).send("E-mail ou senha incorreta");
+
         } catch (err) {
             console.log(err);
             res.status(500).send("Erro ao autenticar usuário");
